449235a45225e2c10494700a96fc60698760bf04
DAN™ <dranger003@gmail.com>
2024-03-26 14:00:36 -0400

    Update 1.10.32.0.

--- inc/console.h
+++ inc/console.h
@@ -6,9 +6,8 @@ namespace game
 	{
 	public:
 		static void printf(const char* fmt, ...) {
-			// 
-			static auto p1 = RelocAddr<std::uintptr_t*>(0x06532650); // 0x06581A10
-			static auto p2 = RelocAddr<std::uintptr_t(*)(std::uintptr_t, const char*, va_list)>(0x0286EF28); // 0x028D8B18
+			static auto p1 = RelocAddr<std::uintptr_t*>(0x06533550); // 0x06532650
+			static auto p2 = RelocAddr<std::uintptr_t(*)(std::uintptr_t, const char*, va_list)>(0x0286F1D8); // 0x0286EF28
 
 			if (*p1 && p2) {
 				auto args = va_list();
@@ -19,7 +18,7 @@ namespace game
 		}
 
 		static std::uintptr_t execute(const char* cmd) {
-			static auto p1 = RelocAddr<std::uintptr_t(*)(std::double_t, const char*)>(0x028694B4); // 0x028D30A4
+			static auto p1 = RelocAddr<std::uintptr_t(*)(std::double_t, const char*)>(0x02869764); // 0x028694B4
 			return p1 ? (*p1)(0.0, cmd) : 0;
 		}
 	};
